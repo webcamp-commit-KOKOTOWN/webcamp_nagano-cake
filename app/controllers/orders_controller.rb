7cc0fdfda3c175d2749604515f6a268aee4585f7
EC2 Default User <ec2-user@ip-172-31-14-181.ec2.internal>
2021-05-22 08:25:23 +0000

    [Change]item, order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,19 +2,19 @@ class OrdersController < ApplicationController
 
   def new
   end
-  
+
   def confirm
   end
-  
+
   def create
   end
 
   def complete
   end
-  
+
   def index
   end
-  
+
   def show
   end
 
