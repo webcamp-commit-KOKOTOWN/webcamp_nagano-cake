58319e288931cde8a900c5cbc9a28667214656d0
EC2 Default User <ec2-user@ip-172-31-14-181.ec2.internal>
2021-05-22 05:44:46 +0000

    [Add]new action and view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,22 @@
 class OrdersController < ApplicationController
 
   def new
+    @user = User.find(current_user.id)
+    @order = Order.new
   end
-  
+
   def confirm
   end
-  
+
   def create
   end
 
   def complete
   end
-  
+
   def index
   end
-  
+
   def show
   end
 
