95304fa9caee4684a9fca62006069cb91308d2f3
tatsumi1996 <dannsamyu@gmail.com>
2021-05-23 06:07:21 +0000

    add_customer_view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,17 @@
 class ItemsController < ApplicationController
+    
     def index
+        @items = Item.where(sale_status: true).page(params[:page]).per(8)
+        @genres = Genre.all
     end
     
     def show
+        @items = Item.all
+        @item = Item.find(params[:id])
+        @cart_item = CartItem.new
+    end
+    
+    def item_params
+            params[:item].permit(:image, :name, :description, :price, :sale_status)
     end
 end
