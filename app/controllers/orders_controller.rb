0ba24af3aa8bc05722980687a785a0f8182edfac
EC2 Default User <ec2-user@ip-172-31-14-181.ec2.internal>
2021-05-23 06:46:12 +0000

    [Change]ラジオボタンによる入力情報の選択

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,27 @@
 class OrdersController < ApplicationController
 
   def new
-    @customer = Customer.find(1)
+    @customer = Customer.find(current_customer.id)
     @order = Order.new
   end
 
   def confirm
+    @customer = Customer.find(current_customer.id)
+    @order = Order.new(order_params)
+    if params[:address_select] == "current"
+      @order.postal_code = @customer.postal_code
+      @order.address = @customer.address
+      @order.name = "#{@customer.last_name}#{@customer.first_name}"
+    elsif params[:address_select] == "registered"
+      address = Address.find(params[:address_registered].to_i)
+      @order.postal_code = address.postal_code
+      @order.address = address.address
+      @order.name = address.name
+    elsif params[:address_select] == "other"
+      @order.postal_code = params[:postal_code]
+      @order.address = params[:address]
+      @order.name = params[:name]
+    end
   end
 
   def create
@@ -18,6 +34,13 @@ class OrdersController < ApplicationController
   end
 
   def show
+  end
+
+
+  private
+
+  def order_params
+    params.permit(:name, :postal_code, :address, :payment_option,)
   end
 
 end
