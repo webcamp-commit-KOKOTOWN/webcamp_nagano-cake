85629c6dbe7a130fbae3b600a61a9e89038ee476
tatsumi1996 <dannsamyu@gmail.com>
2021-05-24 22:51:31 +0000

    add_admin_item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_items, dependent: :destroy
   attachment :image
+  
+  validates :genre_id, :name, :price, presence: true
+	validates :description, length: {maximum: 200}
+	validates :price, numericality: { only_integer: true }
 end
