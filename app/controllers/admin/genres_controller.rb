bec3bccb9bc537adb23629c8f320b9a8c826644f
EC2 Default User <ec2-user@ip-172-31-69-154.ec2.internal>
2021-05-24 02:07:53 +0000

    second

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,18 +1,28 @@
 class Admin::GenresController < ApplicationController
+  before_action :authenticate_admin!
     def index
-        @genre =Genre.new 
+        @genre = Genre.new
         @genres = Genre.all
     end
     
     def create
-        @genre = genre.new(genre_params)
+        genre = Genre.new(genre_params)
+        genre.save
+        redirect_to admin_genres_path
     end
     
     def edit
-        @genre = genre.find(params[:id])
+        @genre = Genre.find(params[:id])
     end
     
     def update
+        @genre = Genre.find(params[:id])
+        if @genre.update(genre_params)
+         flash[:notice] = "ジャンル名を変更しました"
+         redirect_to admin_genres_path
+        else
+           render :edit
+        end
     end
     
     private
