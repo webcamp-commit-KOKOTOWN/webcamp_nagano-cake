58dd1ced957f20c4a43e38b844715e768f2ee155
EC2 Default User <ec2-user@ip-172-31-14-181.ec2.internal>
2021-05-25 09:26:55 +0000

    [Add]注文ステータスの更新機能

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,4 +7,17 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_items = @order.order_items.all
   end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admin_order_path(@order.id)
+  end
+
+
+  private
+
+  def order_params
+    params.permit(:order_status)
+  end
 end
