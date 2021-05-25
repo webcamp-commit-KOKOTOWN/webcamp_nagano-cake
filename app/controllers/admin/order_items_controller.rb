123c2965098c7c5346606b8930e0477f77e79ff9
EC2 Default User <ec2-user@ip-172-31-14-181.ec2.internal>
2021-05-25 09:37:20 +0000

    [Add]制作ステータス更新機能

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,2 +1,14 @@
 class Admin::OrderItemsController < ApplicationController
+  def update
+    @order_item = OrderItem.find(params[:id])
+    @order_item.update(params_order_item)
+    redirect_to admin_order_path(params[:order_id])
+  end
+
+
+  private
+
+  def params_order_item
+    params.permit(:production_status)
+  end
 end
