83b67863cb91857a9056db9869ff274108ed81b6
tatsumi1996 <dannsamyu@gmail.com>
2021-05-25 12:47:54 +0000

    add-admin&&add-cart-items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_055516) do
+ActiveRecord::Schema.define(version: 2021_05_24_155326) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
