7c748f3456a2c1d9684bae241cacd8fb2eff53e0
EC2 Default User <ec2-user@ip-172-31-14-181.ec2.internal>
2021-05-25 09:04:49 +0000

    [Add]管理者側注文履歴一覧・詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,10 @@
 class Admin::OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items.all
+  end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,9 @@ class OrderItem < ApplicationRecord
   belongs_to :item
 
   attribute :production_status, :integer, default: 0
+
+  def status_name
+    name = {0 => "着手不可", 1 => "制作待ち", 2 => "制作中", 3 =>"制作完了"}
+    return name[self.production_status]
+  end
 end
