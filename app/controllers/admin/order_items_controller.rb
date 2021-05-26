7cd9c8e9fdd2ed7ce141fe6d7ca4b885b9c11633
EC2 Default User <ec2-user@ip-172-31-14-181.ec2.internal>
2021-05-26 05:44:19 +0000

    [Change]ステータス更新に伴うフラッシュメッセージの追加

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -2,6 +2,7 @@ class Admin::OrderItemsController < ApplicationController
   def update
     @order_item = OrderItem.find(params[:id])
     @order_item.update(params_order_item)
+    flash[:notice] = "制作ステータスが更新されました"
     if @order_item.production_status == 2
       @order_item.order.update(order_status: 2)
     elsif @order_item.production_status == 3
@@ -15,6 +16,7 @@ class Admin::OrderItemsController < ApplicationController
       if finished == true
         order = @order_item.order
         order.update(order_status: 3)
+        flash[:notice] = "全ての商品の制作が完了したため、注文ステータスを更新しました"
       end
     end
     redirect_to admin_order_path(params[:order_id])
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -11,9 +11,11 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
+    flash[:notice] = "注文ステータスを更新しました"
     if @order.order_status == 1
       @order.order_items.each do |order_item|
         order_item.update(production_status: 1)
+        flash[:notice] = "入金が確認できましたので、制作ステータスを更新しました"
       end
     end
     redirect_to admin_order_path(@order.id)
