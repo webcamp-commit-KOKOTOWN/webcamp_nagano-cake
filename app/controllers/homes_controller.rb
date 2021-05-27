f625488ba604900c5850d432385315c18fea0376
tatsumi1996 <dannsamyu@gmail.com>
2021-05-27 03:21:54 +0000

    トップ、Abou追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,7 @@
 class HomesController < ApplicationController
   def top
+        @items = Item.where(sale_status: true).page(params[:page]).per(8)
+        @genres = Genre.all
   end
 
   def about
